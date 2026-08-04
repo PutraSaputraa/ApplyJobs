399d72d0461a75084cd387d70df0d3fe265cde26
PutraSaputraa <161487538+PutraSaputraa@users.noreply.github.com>
2026-08-04 14:07:11 +0700

    commit

--- ApplyJobs/src/services/firebase.js
+++ ApplyJobs/src/services/firebase.js
@@ -2,12 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const defaultFirebaseConfig = {
+  apiKey: "AIzaSyD99n6cW5DN8ROKxhKNvTDTngzOTn1Wjgs",
+  authDomain: "applyjobs-8cfcf.firebaseapp.com",
+  projectId: "applyjobs-8cfcf",
+  messagingSenderId: "302692499188",
+  appId: "1:302692499188:web:f3e396f2143e6f1fdf68a1",
+};
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || defaultFirebaseConfig.apiKey,
+  authDomain:
+    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
+    defaultFirebaseConfig.authDomain,
+  projectId:
+    import.meta.env.VITE_FIREBASE_PROJECT_ID || defaultFirebaseConfig.projectId,
+  messagingSenderId:
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
+    defaultFirebaseConfig.messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || defaultFirebaseConfig.appId,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
